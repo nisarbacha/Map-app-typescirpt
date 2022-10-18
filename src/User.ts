7cf081efa650640ba84900fde53bbcbb2bd7d624
Nisar <nisar.a@mblhightech.com>
2022-10-18 10:17:50 +0300

    Design Patterns with Typescript

---
+++ src/User.ts
@@ -0,0 +1,20 @@
+ import  faker  from "faker";
+ import Mappable from "./Mappable";
+ export class User implements Mappable {
+   name: string;
+   color:string
+   location: {
+     lat: number;
+     lng: number;
+   };
+   constructor() {
+     this.name = faker.name.firstName();
+     this.location = {
+       lat: parseFloat(faker.address.latitude()),
+       lng: parseFloat(faker.address.longitude()),
+     };
+   }
+   markerContent(): string {
+     return `UserName ${this.name}`;
+   }
+ }
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { CustomMap } from "./CustomMap";
+import { User } from "./user";
+import { Company } from "./Company";
+const customMap = new CustomMap('map');
+const  user = new User();
+const company = new Company();
+customMap.addMarker(user);
+customMap.addMarker(company);
